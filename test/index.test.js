43ac4cc6b3bfdc6c4070a0c35f7ddc05f46fc93c
adesh-nsurely <adesh@nsurely.com>
2022-11-29 17:46:53 +0530

    env

--- test/index.test.js
+++ test/index.test.js
@@ -1,9 +1,7 @@
 import "regenerator-runtime/runtime";
 import motorJS from "../dist/node";
+// Make sure .env file is present in the root directory of the repository. If not, create one based on the env.example file.
 require("dotenv").config();
-
-// Make sure env.json file is present in the root directory of the repository. If not, create one based on the env.example.json file.
-// import * as env from "../env.json";
 
 describe("Auth", () => {
 	it("should be defined", () => {
