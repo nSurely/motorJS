c224f25d8a0e858da44af52aa1d83c6ffcbcd02d
adesh-nsurely <adesh@nsurely.com>
2022-11-29 13:19:18 +0530

    fixed test

--- test/index.test.js
+++ test/index.test.js
@@ -1,20 +1,20 @@
 import 'regenerator-runtime/runtime'
-import { motorAuth } from "../src/index";
+import motorJS from "../dist/node";
 
 // Make sure env.json file is present in the root directory of the repository. If not, create one based on the env.example.json file.
 import * as env from '../env.json';
 
 describe("motorAuth", () => {
 	it("should be defined", () => {
-		expect(motorAuth).toBeDefined();
+		expect(motorJS.Auth).toBeDefined();
 	});
     it("should be able to create a new instance", () => {
-        let auth = new motorAuth({
-            orgId: env.orgId,
-            region: env.region,
-            email: env.email,
-            password: env.password,
-            accountType: env.accountType,
+        let auth = new motorJS.Auth({
+            orgId: env.init.orgId, // <-- Replace with your organisation ID
+            region: env.init.region, // <-- Replace with your region
+            email: env.auth.email, // <-- Replace with users/drivers email
+            password: env.auth.password, // <-- Replace with users/drivers password
+            accountType: env.auth.accountType, // <-- Can be either "user" or "driver"
         });
     });
 });
