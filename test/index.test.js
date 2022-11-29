5ac02b7fc9209c9843814bb7d2a1127adf5a6cb9
adesh-nsurely <adesh@nsurely.com>
2022-11-29 16:35:57 +0530

    env vars added

--- test/index.test.js
+++ test/index.test.js
@@ -2,7 +2,7 @@ import "regenerator-runtime/runtime";
 import motorJS from "../dist/node";
 
 // Make sure env.json file is present in the root directory of the repository. If not, create one based on the env.example.json file.
-import * as env from "../env.json";
+// import * as env from "../env.json";
 
 describe("Auth", () => {
 	it("should be defined", () => {
@@ -10,11 +10,11 @@ describe("Auth", () => {
 	});
 	it("should be able to create a new instance", () => {
 		let auth = new motorJS.Auth({
-			orgId: env.init.orgId, // <-- Replace with your organisation ID
-			region: env.init.region, // <-- Replace with your region
-			email: env.auth.email, // <-- Replace with users/drivers email
-			password: env.auth.password, // <-- Replace with users/drivers password
-			accountType: env.auth.accountType, // <-- Can be either "user" or "driver"
+			orgId: process.env.ORG_ID, // <-- Replace with your organisation ID
+			region: process.env.REGION, // <-- Replace with your region
+			email: process.env.USER_EMAIL, // <-- Replace with users/drivers email
+			password: process.env.USER_EMAIL, // <-- Replace with users/drivers password
+			accountType: 'user', // <-- Can be either "user" or "driver"
 		});
 		auth.login()
 			.then((res) => {
